275fdc7b26fe118e357ea25fdb2077a3745f361f
fbzr <fabricio.spb@gmail.com>
2020-12-03 15:10:22 -0800

    create routes

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const server = express();
 
 server.get("/", (req, res) => res.send("API running"));
 
+const routes = require("./routes");
+
+console.log(routes);
+server.use("/auth", routes.auth);
+server.use("/lists", routes.lists);
+
 const PORT = 8000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
