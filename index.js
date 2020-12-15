7ee54db82f88f9ca4a1a1219060adba6e120d12c
fbzr <fabricio.spb@gmail.com>
2020-12-14 16:21:31 -0800

    refactor index.js. create init async function

--- index.js
+++ index.js
@@ -4,27 +4,30 @@ const helmet = require("helmet");
 const initDb = require("./data/db");
 
 const app = express();
+const http = require("http").createServer(app);
+const routes = require("./routes");
+
 app.use(express.json());
 app.use(helmet());
 
-const routes = require("./routes");
-
-initDb()
-  .then((dbs) => {
-    console.log("MongoDB connected");
+const init = async () => {
+  try {
+    const dbs = await initDb();
     const db = dbs[process.env.NODE_ENV];
-
-    // routes function returns app instance
-    const server = require("http").createServer(routes(app, db));
-
-    const PORT = process.env.PORT || 8000;
-
-    server.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
+    console.log("MongoDB connected");
+  } catch (error) {
     console.error("Failed to connect to Database");
     console.error(err);
     process.exit(1);
+  }
+
+  // routes function returns app instance
+  const server = require("http").createServer(routes(app, db));
+  const PORT = process.env.PORT || 8000;
+
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+};
+
+init();
